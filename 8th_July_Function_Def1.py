ce3ff3779a3908206fcac80f73af5fdb401aa288
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-08 18:40:39 +0530

    Add files via upload

---
+++ 8th_July_Function_Def1.py
@@ -0,0 +1,16 @@
+def triangle():
+    print('triange')
+
+def square():
+    print('square')
+
+def main():
+    print()
+    triangle()
+    print()
+    square()
+
+if __name__ == '__main__':
+    main()
+
+print('\nEnd of program \n')
