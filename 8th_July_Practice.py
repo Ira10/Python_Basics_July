cd70b0cbf5f1939a047afa5582a13400836fb28a
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-08 21:43:31 +0530

    Add files via upload

---
+++ 8th_July_Practice.py
@@ -0,0 +1,13 @@
+# from July_8th_Function_Def1.py import main
+
+# print(July_8th_Function_Def1.main())
+
+# from geometry import main 
+
+# print(main())
+
+def areaOfRectange(l,b):
+    area = l * b 
+    return area 
+
+print(areaOfRectange(5,8))
