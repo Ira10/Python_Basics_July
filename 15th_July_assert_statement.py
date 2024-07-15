0be1df2f0a3d1540257e36466f1ed771e0d13776
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-15 22:47:23 +0530

    Add files via upload

---
+++ 15th_July_assert_statement.py
@@ -0,0 +1,17 @@
+def percent(marks, MaxMarks):
+    perc = (marks /MaxMarks) * 100
+    return perc 
+
+def main():
+    MaxMarks = float(input("Enter maximun marks "))
+    assert MaxMarks >=0 and MaxMarks <= 500
+
+    marks = float(input("Enter maximun marks "))
+    assert marks >=0 and marks <= 500
+
+    percentage = percent(marks, MaxMarks)
+    # return percentage -- it is not working here
+    print("percentage is ", percentage)
+
+if __name__ == "__main__":
+    main()
