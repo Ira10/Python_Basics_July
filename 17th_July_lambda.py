89a8fe2327f4ef63b0c833100982b799257fa881
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-17 10:28:40 +0530

    Add files via upload

---
+++ 17th_July_lambda.py
@@ -0,0 +1,15 @@
+x = lambda a: a + b 
+b = int(input("Enter b : "))
+c = int(input("Enter c : "))
+print(x(c))
+
+
+'''
+(base) indrani.sarkar@HO-002-LP-00389 Python Basics - July % /usr/bin/python
+3 "/Users/indrani.sarkar/Python Basics /Python Basics - July/17th_July_lambd
+a.py"
+Enter b : 3
+Enter c : 283
+286
+
+'''
