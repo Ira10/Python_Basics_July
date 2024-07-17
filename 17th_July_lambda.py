ab11a3ab6434aac53b90504d7c3cecba0dd4913f
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-17 10:32:04 +0530

    Add files via upload

--- 17th_July_lambda.py
+++ 17th_July_lambda.py
@@ -1,7 +1,7 @@
-x = lambda a: a + b 
-b = int(input("Enter b : "))
-c = int(input("Enter c : "))
-print(x(c))
+# x = lambda a: a + b 
+# b = int(input("Enter b : "))
+# c = int(input("Enter c : "))
+# print(x(c))
 
 
 '''
@@ -11,5 +11,19 @@ a.py"
 Enter b : 3
 Enter c : 283
 286
+
+'''
+
+x = lambda : a + b 
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+c = int(input("Enter c : "))
+print(x(c))
+
+'''
+Traceback (most recent call last):
+  File "/Users/indrani.sarkar/Python Basics /Python Basics - July/17th_July_lambda.py", line 21, in <module>
+    print(x(c))
+TypeError: <lambda>() takes 0 positional arguments but 1 was given
 
 '''
