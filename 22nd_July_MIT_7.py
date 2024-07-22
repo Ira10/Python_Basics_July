9f1c4e4b5ad38e809901da8cf71ae82e3541c5c1
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-22 21:24:53 +0530

    Add files via upload

--- 22nd_July_MIT_7.py
+++ 22nd_July_MIT_7.py
@@ -1,10 +1,21 @@
-def is_even(i):
-    if i%2 == 0:
-        return True 
-    else:
-        return False
+# def is_even(i):
+#     if i%2 == 0:
+#         return True 
+#     else:
+#         return False
     
 
+
+# is_even(3) ## this is not getting printed, bc it is just getting returned
+# print(is_even(8))
+# print(is_even(5))
+
+
+
+############################## new #################################
+
+def is_even(i):
+    return i%2 == 0    
 
 is_even(3) ## this is not getting printed, bc it is just getting returned
 print(is_even(8))
